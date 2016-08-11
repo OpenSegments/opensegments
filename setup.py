da958c0ad013f1935ca84ca90476aee6a12c5e4b
Stuart Lynn <stuart.lynn@gmail.com>
2016-08-11 14:49:27 -0400

    intal commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='opensegments',
+      version = '0.0.1',
+      description = 'A module for generating geodemographic segments for census arround the world',
+      url = 'http://github.com/opensegments/opensegments',
+      author = 'Stuart Lynn',
+      author_email = 'stuart@carto.com',
+      license ='MIT',
+      packages=['opensegments'],
+      zip_safe = False)
